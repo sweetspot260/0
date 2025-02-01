2f8b3d13f00b538ae423b0be80107acbbb3e2c62
sweetspot260 <sweetspot00002@gmail.com>
2025-02-01 11:52:40 -0800

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (localStorage.getItem("username")) {
-        window.location.href = "../htmlpages/home.html"; // Redirect if already logged in
+        window.location.href = "/0/htmlpages/home.html"; // Redirect if already logged in
     }
 
     document.querySelector("#loginBtn").addEventListener("click", function () {
         let username = document.getElementById("username").value;
         if (username) {
             localStorage.setItem("username", username);
-            window.location.href = "../htmlpages/home.html"; // Go to home page
+            window.location.href = "/0/htmlpages/home.html"; // Go to home page
         }
     });
 });
